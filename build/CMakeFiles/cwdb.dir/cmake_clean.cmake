8b0a6e44233e35fc9400a64f958fe90e3342895c
Губенко Константин <kstgnbk@gmail.com>
2026-05-11 15:48:54 +0300

    Тест Курсовой

---
+++ build/CMakeFiles/cwdb.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cwdb.dir/src/main.cpp.obj"
+  "CMakeFiles/cwdb.dir/src/main.cpp.obj.d"
+  "cwdb.exe"
+  "cwdb.exe.manifest"
+  "cwdb.pdb"
+  "libcwdb.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cwdb.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
